2d476ae6cad0f6afbcfdcec80be35a0093032b27
A S Siddarth <79092592+Ashera2004@users.noreply.github.com>
2026-04-28 09:40:08 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -1,39 +1,43 @@
-from collections import deque
 from collections import defaultdict
+import networkx as nx
+import matplotlib.pyplot as plt
 
+graph=defaultdict(list)
+G=nx.Graph()
+nodes,edges=map(int,input().split())
+for i in range(edges):
+    u,v=map(str,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    G.add_edge(u,v)
+nx.draw(G, with_labels=True, node_color="lightblue", edge_color="red", width=2, node_size=2000)
+plt.show()
+print(graph)
 
-'''
-V E
-FOR EVERY EDGE
-U V
-7 9
-A B
-A C
-A F
-C E
-C F
-C D
-D E
-D G
-G F
-'''
-def bfs(graph,start,visited,path):
-    queue = deque()
-    path.append(start)
-    queue.append(start)
-    visited[start] = True
-    while len(queue) != 0:
-        tmpnode = queue.popleft()
-        #TYPE UR CODE HERE
-    return path
+#Breadth First Search
+from collections import deque
+def bfs(graph, start, visited, path):
+  ''' Uses Queue and iteration
+    Complete
+    Time Complexity and Space Complexity: B^D(Exponential)
+    B-Branching Factor, D-Depth'''
+  queue = deque()
+  path.append(start)
+  queue.append(start)
+  while len(queue)!=0:
+    tmpnode=queue.popleft()
+    for neighbour in graph[tmpnode]:
+      if visited[neighbour]==False:
+        path.append(neighbour)
+        queue.append(neighbour)
+        visited[neighbour]=True
+  return path
 
-graph = defaultdict(list)
-v,e = map(int,input().split())
-for i in range(e):
-    #TYOE UR CODE HERE
+start = input()
 
-start = '0'
 path = []
 visited = defaultdict(bool)
-traversedpath = bfs(graph,start,visited,path)
-print(traversedpath)
+traversepath = bfs(graph, start, visited, path)
+print("Breadth First Search:")
+print(traversepath)
+
